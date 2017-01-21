92289b712ca33ff1524cf503994ca0eb4033b56f
Miguel Coronel <m.coronel.seg@gmail.com>
2017-01-21 01:30:01 -0600

    Redirects User when a Project is not found

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,6 @@
 class ProjectsController < ApplicationController
+	before_action :set_project, only: [:show, :edit, :update, :destroy]
+
 	def index
 		@projects = Project.all
 	end
@@ -20,12 +22,9 @@ class ProjectsController < ApplicationController
 	end
 
 	def edit
-		@project = Project.find params[:id]
 	end
 
 	def update
-		@project = Project.find params[:id]
-
 		if @project.update project_params
 			flash[:notice] = "Project has been updated"
 			redirect_to @project
@@ -36,11 +35,9 @@ class ProjectsController < ApplicationController
 	end
 
 	def show
-		@project = Project.find params[:id]
 	end
 
 	def destroy
-		@project = Project.find params[:id]
 		@project.destroy
 
 		flash[:notice] = "Project has been deleted"
@@ -51,5 +48,13 @@ class ProjectsController < ApplicationController
 
 	def project_params
 		params.require(:project).permit(:name, :description)
+	end
+
+	def set_project
+		@project = Project.find params[:id]
+
+	rescue ActiveRecord::RecordNotFound
+		flash[:alert] = "The project you were looking for could not be found"
+		redirect_to projects_path
 	end
 end
--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -2,4 +2,12 @@ require 'rails_helper'
 
 RSpec.describe ProjectsController, type: :controller do
 
+	it "handle a missing project correclty" do
+		get :show, id: "invalid_id"
+
+		expect(response).to  redirect_to projects_path
+
+		message = "The project you were looking for could not be found"
+		expect(flash[:alert]).to eq message
+	end
 end
