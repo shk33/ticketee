0e6e08035114236c0adc789d8e3437b5662fcf23
Miguel Coronel <m.coronel.seg@gmail.com>
2017-01-21 00:31:48 -0600

    Adds edit project page and actions

--- spec/features/viewing_projects_spec.rb
+++ spec/features/viewing_projects_spec.rb
@@ -1,15 +1,31 @@
 require "rails_helper"
 
-RSpec.feature "User can create view projects" do
+RSpec.feature "User can create new projects" do
 
-	scenario "with the project details" do
-		project = FactoryGirl.create(:project, name: "Sublime Text 3")
-
+	before do
 		visit "/"
-		click_link "Sublime Text 3"
+		click_link "New Project"
+	end
 
+	scenario "with valid attributes" do
+		fill_in "Name", with: "Sublime Text 3"
+		fill_in "Description", with: "A good code editor"
+		click_button "Create Project"
+
+		expect(page).to  have_content "Project has been created"
+
+		project = Project.find_by(name: "Sublime Text 3")
 		expect(page.current_url).to eq project_url(project)
+
+		title = "Sublime Text 3 - Projects - Ticketee"
+		expect(page).to have_title title
 
 	end
 
+	scenario "when providing invalid attributes" do
+		click_button "Create Project"
+
+		expect(page).to have_content "Project has not been created."
+		expect(page).to have_content "Name can't be blank"
+	end
 end
