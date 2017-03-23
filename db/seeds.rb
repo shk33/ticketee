777d2320430d8ebaeafc672745cb5ef80c326cd9
Miguel Coronel <m.coronel.seg@gmail.com>
2017-03-23 00:04:03 -0600

    Add seeders for admin user, normal user and projects

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+unless User.exists? email: "admin@ticktee.com"
+	User.create! email: "admin@ticktee.com", password: "password", admin: true
+end
+
+unless User.exists? email: "viewer@ticktee.com"
+	User.create! email: "viewer@ticktee.com", password: "password"
+end
+
+["Sublime Text 3", "Internet Explorer"].each do |name|
+	unless Project.exists?(name: name)
+		Project.create!(name: name, description: "A sample project about #{name}"
+			)
+	end
+end
