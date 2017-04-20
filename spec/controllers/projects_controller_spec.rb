d78941e6a2ef3b3e7a228966d40f43b48428b628
Miguel Coronel <m.coronel.seg@gmail.com>
2017-04-20 01:10:05 -0500

    when an authorization error raises it handles by redirecting to root url

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -2,12 +2,23 @@ require 'rails_helper'
 
 RSpec.describe ProjectsController, type: :controller do
 
-	it "handle a missing project correclty" do
+	it "handles a missing project correctly" do
 		get :show, id: "invalid_id"
 
 		expect(response).to  redirect_to projects_path
 
 		message = "The project you were looking for could not be found"
+		expect(flash[:alert]).to eq message
+	end
+
+	it "handles permission errors by redirecting to a safe place" do
+		allow(controller).to receive(:current_user)
+
+		project = FactoryGirl.create(:project)
+		get :show, id: project
+
+		expect(response).to redirect_to(root_url)
+		message = "You aren't allowed to do that."
 		expect(flash[:alert]).to eq message
 	end
 end
