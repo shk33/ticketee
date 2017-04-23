344d31d6afef1f28a6c20a7845d8b40df2b94db1
Miguel Coronel <m.coronel.seg@gmail.com>
2017-04-22 23:58:04 -0500

    Dont list projects for users who don't have permission to see them

--- spec/features/projects/viewing_projects_spec.rb
+++ spec/features/projects/viewing_projects_spec.rb
@@ -1,17 +1,23 @@
 require "rails_helper"
 
 RSpec.feature "User can view projects" do
-	let(:user)    { FactoryGirl.create(:user) } 
-	let(:project) { FactoryGirl.create(:project, name: "Internet Explorer") } 
+  let(:user)    { FactoryGirl.create(:user) }
+  let(:project) { FactoryGirl.create(:project, name: "Internet Explorer") }
 
-	before do
-		assign_role! user, :viewer, project
-		login_as user
-		visit project_url(project)
-	end
+  before do
+    assign_role! user, :viewer, project
+    login_as user
+    visit project_url(project)
+  end
 
-	scenario "specific project" do
-		expect(page).to have_content project.name
-		expect(page).to have_content project.description
-	end
+  scenario "specific project" do
+    expect(page).to have_content project.name
+    expect(page).to have_content project.description
+  end
+
+  scenario "unless they do not have the permissions" do
+    FactoryGirl.create(:project, name: "Hidden")
+    visit "/"
+    expect(page).not_to have_content "Hidden"
+  end
 end
