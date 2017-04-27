0968cc31e7e704ec8dd283da379af45678f08cb5
Miguel Coronel <m.coronel.seg@gmail.com>
2017-04-27 00:02:19 -0500

    Refactors code for policies

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,19 @@
 class Project < ActiveRecord::Base
-	validates :name, presence: true
+  @@roles = [:manager, :editor, :viewer]
 
-	has_many :tickets, dependent: :delete_all
-	has_many :roles,   dependent: :delete_all
+  validates :name, presence: true
+
+  has_many :tickets, dependent: :delete_all
+  has_many :roles,   dependent: :delete_all
+
+
+  def has_member? user
+    roles.exists? user_id: user
+  end
+
+  @@roles.each do |role|
+    define_method "has_#{role}?" do |user|
+      roles.exists?(user_id: user, role: role)
+    end
+  end
 end
