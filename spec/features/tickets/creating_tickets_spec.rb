733486f644bc8aab2f4b887ec984b0932669729a
Miguel Coronel <m.coronel.seg@gmail.com>
2017-04-27 00:15:21 -0500

    Allows editor, managers and admin to create tickets

--- spec/features/tickets/creating_tickets_spec.rb
+++ spec/features/tickets/creating_tickets_spec.rb
@@ -1,13 +1,13 @@
 require "rails_helper"
 
 RSpec.feature "Users can create new tickets" do
-	let(:user) { FactoryGirl.create(:user) } 
-	
+	let(:user) { FactoryGirl.create(:user) }
+
 	before do
 		login_as(user)
 		project = FactoryGirl.create(:project, name: "Internet Explorer")
-		assign_role! user, :viewer, project
-		
+		assign_role! user, :editor, project
+
 		visit project_path(project)
 		click_link "New Ticket"
 	end
@@ -35,7 +35,7 @@ RSpec.feature "Users can create new tickets" do
 		fill_in "Name", with: "Non-standards compliance"
 		fill_in "Description", with: "Short!"
 		click_button "Create Ticket"
-		
+
 		expect(page).to have_content "Ticket has not been created."
 		expect(page).to have_content "Description is too short"
 	end
