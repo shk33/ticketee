55ea90c70647ddc52b5b9223a97b3213598cadf9
Miguel Coronel <m.coronel.seg@gmail.com>
2017-06-06 00:59:55 -0500

    when a user is created it nows assigns their roles

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -34,12 +34,7 @@ class Admin::UsersController < Admin::ApplicationController
 
     User.transaction do
       @user.roles.clear
-      role_data = params.fetch(:roles, [])
-      role_data.each do |project_id, role_name|
-        if role_name.present?
-          @user.roles.build(project_id: project_id, role: role_name)
-        end
-      end
+      build_roles_for @user
 
       if @user.update user_params
         flash[:notice] = "User has been updated."
@@ -54,7 +49,8 @@ class Admin::UsersController < Admin::ApplicationController
   end
 
   def create
-  	@user = User.new user_params
+    @user = User.new user_params
+    build_roles_for @user
 
   	if @user.save
   		flash[:notice] = "User has been created."
@@ -66,6 +62,15 @@ class Admin::UsersController < Admin::ApplicationController
   end
 
   private
+
+    def build_roles_for user
+      role_data = params.fetch(:roles, [])
+      role_data.each do |project_id, role_name|
+        if role_name.present?
+          @user.roles.build(project_id: project_id, role: role_name)
+        end
+      end
+    end
 
     def user_params
       params.require(:user).permit(:email, :password, :admin)
