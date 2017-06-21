97eccba3ddb69e071addba641819c989da1b0ee4
Miguel Coronel <m.coronel.seg@gmail.com>
2017-06-21 01:29:46 -0500

    Adds cache file uploading

--- spec/features/tickets/creating_tickets_spec.rb
+++ spec/features/tickets/creating_tickets_spec.rb
@@ -37,6 +37,19 @@ RSpec.feature "Users can create new tickets" do
     end
   end
 
+  scenario "persisting file uploads across form displays" do
+    attach_file "File", 'spec/fixtures/speed.txt'
+    click_button  'Create Ticket'
+
+    fill_in 'Name', with: 'Add documentation for blink tag'
+    fill_in 'Description', with: 'The blink tag has a speed attribute'
+    click_button  'Create Ticket'
+
+    within("#ticket .attachment") do
+      expect(page).to have_content "speed.txt"
+    end
+  end
+
 	scenario "when providing invalid attributes" do
 		click_button "Create Ticket"
 		expect(page).to have_content "Ticket has not been created."
